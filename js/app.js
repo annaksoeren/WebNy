68d337b474bf526836490425b2c30b79d0aa28ed
Anna K Sørensen <annaks21@student.aau.dk>
2024-05-25 19:43:38 +0200

    her

--- js/app.js
+++ js/app.js
@@ -3,13 +3,20 @@ const newZeaLatLng = { lat: -40.851, lng: 172.772};
 const SidneyLatLng = { lat: -33.865, lng: 151.209};
 const FijiLatLng = { lat:  -17.713, lng: 178.065};
 
-async function initMap() {
+function initMap() {
   const { Map } = await google.maps.importLibrary("maps");
 
-  map = new Map(document.getElementById("map"), {
+  map = new google.map.Map(document.getElementById("map"), {
     center: newZeaLatLng,
     zoom: 8,
+    mapID: "c3fe9fd4007ea226",
   });
+
+  const marker = new google.maps.Marker({
+    position: SidneyLatLng,
+    map: map,
+  })
 }
 
 initMap();
+
